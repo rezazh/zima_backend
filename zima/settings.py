7379b5721bd72d8c47fe37a5726d305cca372ec4
rezazh <alirezaarzhangi84562@gmail.com>
2025-05-30 19:48:56 +0330

    fix deploy file

--- zima/settings.py
+++ zima/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.middleware.RequestLogMiddleware',
+    # 'middleware.middleware.RequestLogMiddleware',
 ]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
