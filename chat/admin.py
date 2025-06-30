66840b569578337a0826409579e8a4bf625deafa
rezazh <alirezaarzhangi84562@gmail.com>
2025-06-30 12:30:08 +0330

    fix test

--- chat/admin.py
+++ chat/admin.py
@@ -42,6 +42,7 @@ class UserStatusAdmin(admin.ModelAdmin):
     date_hierarchy = 'last_seen'
 
 
+
 @admin.register(TemporaryFile)
 class TemporaryFileAdmin(admin.ModelAdmin):
     list_display = ('user', 'file', 'created_at')
